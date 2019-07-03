f3842763c2df94de5a57fe2e4a7ad4e68c5b4c54
Cédric Soulas <soulascedric@gmail.com>
2019-07-03 14:37:36 +0200

    Document the cypress test minimal example

--- cypress/integration/home.spec.js
+++ cypress/integration/home.spec.js
@@ -1,9 +1,14 @@
-context("Markdown", () => {
+context("Home page", () => {
   beforeEach(() => {
     cy.visit("/");
   });
 
   it("display the homepage content", () => {
+    /**
+      The Elm app uses data-* attributes to provide context to the selectors
+      and insulate them from CSS or JS changes.
+      https://docs.cypress.io/guides/references/best-practices.html
+    */
     cy.get("[data-test=content]").contains("Elm Batteries Included");
   });
 });
