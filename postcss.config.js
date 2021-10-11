40163905783185de5f803a28e4255344a9d688ca
Cédric Soulas <soulascedric@gmail.com>
2021-10-11 21:31:02 +0200

    Upgrade dependencies to their latest versions, including Tailwind 2.2

--- postcss.config.js
+++ postcss.config.js
@@ -1,24 +1,6 @@
-const autoprefixer = require("autoprefixer");
-const purgecss = require("@fullhuman/postcss-purgecss");
-const tailwindcss = require("tailwindcss")("tailwind.config.js");
-
-const development = {
-  plugins: [tailwindcss, autoprefixer],
+module.exports = {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
 };
-
-const production = {
-  plugins: [
-    tailwindcss,
-    purgecss({
-      content: ["./src/**/*.elm", "index.js"],
-      whitelist: ["html", "body"],
-    }),
-    autoprefixer,
-  ],
-};
-
-if (process.env.NODE_ENV === "production") {
-  module.exports = production;
-} else {
-  module.exports = development;
-}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,11 @@
 module.exports = {
-  purge: {
-    content: ["./src/**/*.elm", "index.js"],
-  },
+  purge: ["./src/**/*.elm", "index.js"],
+  darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
   },
-  variants: {},
+  variants: {
+    extend: {},
+  },
   plugins: [],
 };
